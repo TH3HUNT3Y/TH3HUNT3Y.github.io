62f290af0708ab82855775e07ac05307ba9b90c0
HUNT <121392683+TH3HUNT3Y@users.noreply.github.com>
2025-04-20 03:13:57 -0400

    Create HunterHub.lua

---
+++ scripts/HunterHub.lua
@@ -0,0 +1,67 @@
+-- I dont really care if you are reading this its just VERY messy in the actual scripts itself so im just warning you
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if game.PlaceID == 70876832253163 or 116495829188952 then
+    loadstring(game:HttpGet(https://th3hunt3y.github.io/scripts/deadrails.lua))()
+else
+    if game.PlaceID == 11063612131 then
+    loadstring(game:HttpGet(https://th3hunt3y.github.io/scripts/JumpASecond.lua))()
