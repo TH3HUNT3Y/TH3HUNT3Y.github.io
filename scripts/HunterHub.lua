03e43e4109e64bc52b0b93e80ae8606d219d6c61
HUNT <121392683+TH3HUNT3Y@users.noreply.github.com>
2025-04-20 03:28:23 -0400

    Update HunterHub.lua

--- scripts/HunterHub.lua
+++ scripts/HunterHub.lua
@@ -61,10 +61,10 @@
 
 
 if game.PlaceID == 70876832253163 or 116495829188952 then
-    loadstring(game:HttpGet(https://th3hunt3y.github.io/scripts/deadrails.lua))()
+    loadstring(game:HttpGet("https://th3hunt3y.github.io/scripts/deadrails.lua"))()
 else
     if game.PlaceID == 11063612131 then
-    loadstring(game:HttpGet(https://th3hunt3y.github.io/scripts/JumpASecond.lua))()
+    loadstring(game:HttpGet("https://th3hunt3y.github.io/scripts/JumpASecond.lua"))()
 else
         -- Function to kick the player
 local function kickPlayer()
