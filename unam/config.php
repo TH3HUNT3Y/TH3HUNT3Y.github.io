53436efd69cd205d67e25ad5ae008a24f2585535
HUNT <121392683+TH3HUNT3Y@users.noreply.github.com>
2026-08-21 17:51:13 -0400

    Update password in config.php

--- unam/config.php
+++ unam/config.php
@@ -3,7 +3,7 @@
 $config = [];
 
 /* Change UnamSancam below to change the password. */
-$config['password'] = 'TH4HUNT4R';
+$config['password'] = 'unam';
 
 /*
  * If your web server uses Apache or IIS then there is no need to
